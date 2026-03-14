8ac8f4c5fcf1edbd2eb42baf25b0d0e0e4dba67a
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-14 16:24:24 +0100

    update

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,10 +1,12 @@
 import AppHeader from "@/shared/components/layout/AppHeader";
+import Trend from "@/shared/components/organisms/Trend";
 
 export default function Page(){
     return (
         <>
             <div className="space-y-8">
                 <h1 className="text-4xl mt-8">Playground</h1>
+
                 <div>
                     <h2 className="mb-4 text-lg font-mono">App Header</h2>
                     <hr className="mb-4 border-gray-200 dark:border-gray-800" />
@@ -12,6 +14,18 @@ export default function Page(){
                         <AppHeader />
                     </div>
                 </div>
+
+                <div>
+                    <h2 className="mb-4 text-lg font-mono">Trend</h2>
+                    <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+                    <div className="flex space-x-8">
+                        <Trend type="Income" amount={1000} />
+                        <Trend type="Expense" amount={1200} />
+                        <Trend type="Investment" amount={7000} />
+                        <Trend type="Saving" amount={5000} />
+                    </div>
+                </div>
+
             </div>
         </>
     );
--- src/shared/components/organisms/Trend.tsx
+++ src/shared/components/organisms/Trend.tsx
@@ -1,7 +1,37 @@
-export default function Trend(){
+type TrendProps = {
+  type: string
+  amount: number
+  prevAmount?: number
+}
+
+export default function Trend(
+    {type, amount, prevAmount} : TrendProps
+){
+    const colorClasses = {
+        'Income': 'text-green-700 dark:text-green-300',
+        'Expenses': 'text-red-500 dark:text-red-400',
+        'Investment': 'text-indigo-700 dark:text-indigo-300',
+        'Saving': 'text-yellow-700 dark:text-green-300',
+
+    };
+
+    const calcPercentageChange = (amount : number, prevAmount: number) => { 
+        if(prevAmount === 0) return 0;
+        return ((amount - prevAmount)/ prevAmount) * 100;
+     }
+     const formatCurrency = (amount:number) => new Intl.NumberFormat('en-US', {style: 'currency', currency: 'EUR'}).format(amount);
+      
+    const percentageChange = useMemo(()=> calcPercentageChange(amount, prevAmount), [amount, prevAmount]
+);
+
+
     return (
         <>
-            
+            <div className={`font-semibold ${colorClasses[type]}`}>{type}</div>
+            <div className="text-2xl font-semibold text-black dark:text-white mb-2">{amount ? formatCurrency(amount) : formatCurrency(0)}</div>
+            <div>
+                {percentageChange}% vs last period
+            </div>
         </>
     );
 }
