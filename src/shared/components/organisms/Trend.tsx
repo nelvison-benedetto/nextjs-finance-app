f5edd913875bbabbec312ee659527bfec574d625
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-17 16:44:01 +0100

    update

--- src/shared/components/organisms/Trend.tsx
+++ src/shared/components/organisms/Trend.tsx
@@ -11,6 +11,7 @@ type TrendProps = {
 }
 
 export default function Trend({ type, amount, prevAmount }: TrendProps) {
+
   const colorClasses: Record<TrendType, string> = {
     'Income': 'text-green-700 dark:text-green-300',
     'Expense': 'text-red-500 dark:text-red-400',
@@ -21,20 +22,24 @@ export default function Trend({ type, amount, prevAmount }: TrendProps) {
   const calcPercentageChange = (amount: number, prevAmount: number) => {
     if (!prevAmount || !amount) return 0
     return ((amount - prevAmount) / prevAmount) * 100
-  }
+  }  //calcolo variazione %. ((attuale - precedente) / precedente) * 100
 
   const formattedCurrency = formatCurrency(amount)
 
-  const percentageChange = useMemo(() => calcPercentageChange(amount, prevAmount ?? 0).toFixed(0), [amount, prevAmount])
+  const percentageChange = useMemo(
+    () => calcPercentageChange(amount, prevAmount ?? 0).toFixed(0),  //toFixed(0) arrotonda a 0 decimali
+    [amount, prevAmount]  //ricalcola solo se cambiano amount o prevAmount
+  )
 
   return (
     <div>
-      <div className={`font-semibold ${colorClasses[type]}`}>{type}</div>
+      <div className={`font-semibold ${colorClasses[type]}`}>{type}</div>  {/*cambia coloe DINAMICAMENTE!!*/}
       <div className="text-2xl font-semibold text-black dark:text-white mb-2">{formattedCurrency}</div>
       <div className="flex space-x-1 items-center text-sm">
         <div>
           {Number(percentageChange) <= 0 && <ArrowDownLeft className="text-red-700 dark:text-red-300" />}
           {Number(percentageChange) > 0 && <ArrowUpRight className="text-green-700 dark:text-green-300" />}
+            {/*renderizza freccia o su o giù, a seconda del valore di percentageChange ( <=0 oppure >0 )  */}
           {percentageChange}% vs last period
         </div>
       </div>
