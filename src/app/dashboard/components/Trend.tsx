c715da63174ddf666909ee78f8d037e0c021822d
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-18 15:09:17 +0100

    update

--- src/app/dashboard/components/Trend.tsx
+++ src/app/dashboard/components/Trend.tsx
@@ -4,13 +4,13 @@ import { createClient } from '@/lib/supabase/server'
 export default async function Trend({ type, range }: { type: string; range: string }) {
   const supabase = await createClient()
   const { data, error } = await supabase
-    .rpc('calculate_total', {
+    .rpc('calculate_total', {  //call custom function 'calculate_total' in db supabase e passa params
       range_arg: range,
       type_arg: type
     })
   if (error) throw new Error('Could not fetch the trend data')
 
-  const amounts = data[0]
+  const amounts = data[0]  //estrai i dati (perche .rpc rtirna sempre un Array)
 
   return <BaseTrend type={type as 'Income' | 'Expense' | 'Saving' | 'Investment'} amount={amounts.current_amount} prevAmount={amounts.previous_amount} />
 }
