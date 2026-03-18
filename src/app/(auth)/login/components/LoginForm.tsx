3e2525bbbc0675808bb4e12e9f57f9bf96b747b0
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-18 12:30:29 +0100

    update

--- src/app/(auth)/login/components/LoginForm.tsx
+++ src/app/(auth)/login/components/LoginForm.tsx
@@ -2,15 +2,16 @@
 import Input1 from '@/shared/components/atoms/Input1'
 import SubmitButton from '@/shared/components/molecules/SubmitButton'
 import { login } from '@/lib/actions'
-import { useActionState } from 'react'
+import { useActionState } from 'react'  //collega form->server action, cosi puoi gestire stato risposta (success/error)
 
-const initialState = {
-  message: '',
+const initialState = {  //stato iniziale del form
+  message: '', 
   error: false
 }
 
 export default function LoginForm() {
-  const [state, formAction] = useActionState(login, initialState)
+  const [state, formAction] = useActionState(login, initialState);  //COLLEGA IL FORM ALLA SERVER ACTION (function) 'login'
+
   return <form action={formAction} className="space-y-2">
     <Input1 type="email" placeholder="name@example.com"
       name="email" required />
