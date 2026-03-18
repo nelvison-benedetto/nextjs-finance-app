0b354a68c8871b361093b20da051a102ae7c88a9
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-18 15:52:42 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@/shared/styles/globals.css";
+import "@/styles/globals.css";
 import { getServerTheme, type Theme } from '@/shared/hooks/useServerDarkMode';
 import Providers from './Providers';
 
