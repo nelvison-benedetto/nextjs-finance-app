cdd55d547a4a6fc943e111e4a1110aa56101b60b
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-18 14:06:39 +0100

    update

--- src/app/dashboard/components/Range.tsx
+++ src/app/dashboard/components/Range.tsx
@@ -1,18 +1,19 @@
 'use client'
 import DateRangeSelect from '@/shared/components/atoms/DateRangeSelect'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'  //hooks x lavore w URL
 
 export default function Range({ defaultView }: { defaultView?: string }) {
-  const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const { replace } = useRouter()
+  const searchParams = useSearchParams()  //legge i parametri da url e.g./dashboard?range=last7days se fai searchParams.get('range') ottieni 'last7days'
+  const pathname = usePathname()  //prende il path corrente e.g. '/dashboard'
+  const { replace } = useRouter()  //funzione x cambiare URL, senza ricaricare pagina/aggiungere alla history
   const range = searchParams.get('range') ?? defaultView ?? 'last30days'
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const params = new URLSearchParams()
-    params.set('range', e.target.value)
-    replace(`${pathname}?${params.toString()}`)
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {  //FUNZIONE CHIAMATA QUANDO CAMBIA IL SELECT
+    const params = new URLSearchParams()  //crea nuovi parametri URL
+    params.set('range', e.target.value)    //setti ...?range=valoreSelezionato
+    replace(`${pathname}?${params.toString()}`)  //aggiorni url
   }
 
-  return <DateRangeSelect value={range} onChange={handleChange} />
+  return <DateRangeSelect value={range} onChange={handleChange} />  //value è il valore attuale, onChange è l'handler
+
 }
